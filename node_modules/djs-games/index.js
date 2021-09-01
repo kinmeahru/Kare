8b1ae35cd098a38cadc824d4bee98059010f6ef9
Rytsaa <85903730+Kayeda117@users.noreply.github.com>
2021-09-01 19:14:33 +0200

    Add files via upload

---
+++ node_modules/djs-games/index.js
@@ -0,0 +1,19 @@
+module.exports.SnakeGame = require('./games/snake-game')
+
+module.exports.TicTacToe = require('./games/tictactoe-game')
+
+module.exports.ConnectFour = require('./games/connect4-game')
+
+module.exports.FastTyper = require('./games/fasttyper-game')
+
+module.exports.GuessTheNumber = require('./games/guessTheNumber-game')
+
+module.exports.RockPaperScissors = require('./games/rps-game')
+
+module.exports.Pokemon = require('./games/pokemon-game')
+
+module.exports.HangMan = require('./games/hangman-game')
+
+module.exports.GTL = require('./games/guessTheLogo-game')
+
+module.exports.GTF = require('./games/guessTheFlag-game')
