1dbaabb89905da4c800050c1ff5bbd5cdd8f5bf4
kayedaa <85903730+kayedaa@users.noreply.github.com>
2021-09-03 00:08:35 +0200

    .

--- commands/fun/gpokemon.js
+++ commands/fun/gpokemon.js
@@ -8,10 +8,10 @@ module.exports = {
 
   run: async(client , message , args) => {
 
-const djsGames = require('djs-games')
+
 const game = new Pokemon({
   message: message,
-  token : "zw4ia33qe3uqj9sbolostk19d3wc3mawrhlwt4gzxkobtyu7ho"
+  token : "your dagpi api token" //you can find it here https://dagpi.xyz/dashboard
 })
  game.start(message)
                           
