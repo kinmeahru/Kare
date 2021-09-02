642eaf8dee589d774172cf29d7879ddd58d71f52
Rytsaa <85903730+Kayeda117@users.noreply.github.com>
2021-09-02 14:37:18 +0200

    Add files via upload

---
+++ commands/nsfw/orgy.js
@@ -0,0 +1,68 @@
+const discord = require("discord.js");//
+const akaneko = require('akaneko');
+const { error_color, success_color, warning_color, warning, success, error, music, music_color, volumee, main_color } = require('../../config.json')
+const db = require('quick.db')
+
+module.exports = {
+  name: "orgy",
+  aliases: [],
+  category: "🔞nsfw",
+  description: "Get some wallpapers",
+  run: async (client, message, args) => {
+		var lang = db.get(`language_${message.guild.id}`)
+       if(!lang) lang = 'en'
+
+//english
+if(lang === 'en') {
+    
+    if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} This channel doesn't support nsfw content!`,
+				color : error_color
+			}})
+      
+    } else {
+    
+    let akanekoSan = new discord.MessageEmbed()
+    akanekoSan.setColor(main_color)
+    akanekoSan.setImage(akaneko.nsfw.orgy()).setFooter('Akimi')
+    return message.channel.send(akanekoSan);
+      
+    }
+}
+if(lang == 'it') {
+	if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Questo canale non supporta contenuti nsfw!`,
+				color : error_color
+			}})
+      
+    } else {
+    
+    let akanekoSan = new discord.MessageEmbed()
+    akanekoSan.setColor(main_color)
+    akanekoSan.setImage(akaneko.nsfw.orgy()).setFooter('Akimi')
+    return message.channel.send(akanekoSan);
+      
+    }
+
+}
+if(lang == 'de') {
+	if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Dieser Channel unterstützt keine NSFW-Inhalte`,
+				color : error_color
+			}})
+      
+    } else {
+    
+    let akanekoSan = new discord.MessageEmbed()
+    akanekoSan.setColor(main_color)
+    akanekoSan.setImage(akaneko.nsfw.orgy()).setFooter('Akimi')
+    return message.channel.send(akanekoSan);
+      
+    }
+
+}
+  }
+}
