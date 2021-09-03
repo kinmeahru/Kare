05fb74d9168439ec200cd2c093ded108afdfd954
kayedaa <85903730+kayedaa@users.noreply.github.com>
2021-09-04 01:02:29 +0200

    Add files via upload

---
+++ commands/nsfw/blowjob.js
@@ -0,0 +1,84 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+
+const { error_color, success_color, warning_color, warning, success, error, music, music_color, volumee, main_color, bot_name } = require('../../config.json')
+const db = require('quick.db')
+module.exports = {
+  name: "blowjob",
+  aliases: [],
+  category: "🔞nsfw",
+  description: "Get some wallpapers",
+  run: async (client, message, args) => {
+		var lang = db.get(`language_${message.guild.id}`)
+       if(!lang) lang = 'en'
+
+//english
+if(lang === 'en') {
+    if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} This channel doesn't support nsfw content!`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/blowjob')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Blowjob")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+if(lang === 'it') {
+      if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Questo canale non supporta contenuti nsfw!`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/blowjob')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Pompino")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+if(lang === 'de') {
+      if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Dieser Channel unterstützt keine NSFW-Inhalte`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/blowjob')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Blasen")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+  }
+}
---
+++ commands/nsfw/trap.js
@@ -0,0 +1,109 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+
+const { error_color, success_color, warning_color, warning, success, error, music, music_color, volumee, main_color } = require('../../config.json')
+const db = require('quick.db')
+module.exports = {
+  name: "trap",
+  aliases: [],
+  category: "🔞nsfw",
+  description: "Get some wallpapers",
+  run: async (client, message, args) => {
+		var lang = db.get(`language_${message.guild.id}`)
+       if(!lang) lang = 'en'
+
+//english
+if(lang === 'en') {
+    if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} This channel doesn't support nsfw content!`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/trap')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Trap")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+if(lang === 'it') {
+      if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Questo canale non supporta contenuti nsfw!`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/trap')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Trappola")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+if(lang === 'de') {
+      if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} Dieser Channel unterstützt keine NSFW-Inhalte`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/trap')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Falle")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(bot_name)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+
+
+if(lang === 'owo') {
+    if(!message.channel.nsfw) {
+      return message.reply({embed : {
+				description : `${error} This cwhannel dwoesn't suppowt nsfw cowntent!`,
+				color : error_color
+			}})
+      
+    } else {
+
+    superagent.get('https://nekos.life/api/v2/img/trap')
+    .end((err, response) => {
+  const lewdembed = new Discord.MessageEmbed()
+  .setTitle("Trap")
+  .setImage(response.body.url)
+  .setColor(main_color)
+  .setFooter(`Akimi`)
+  .setURL(response.body.url);
+message.channel.send(lewdembed);
+})
+
+    }
+}
+
+  }
+}
